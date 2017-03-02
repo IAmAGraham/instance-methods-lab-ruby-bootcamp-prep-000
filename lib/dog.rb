168506d5ceb6e6f545eee158059266e5bb1306a7
Andrea Graham <andrea.f.graham@gmail.com>
2017-03-02 18:21:09 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+class dog
+  def bark
+    puts "woof!"
+  end
+
+  def sit
+    puts "The dog is sitting!"
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,9 @@
+class person
+  def talk
+    puts "Hello World!"
+  end
+
+  def walk
+    puts "The person is walking!"
+  end
+end 
